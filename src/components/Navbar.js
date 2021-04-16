3edec71100e096f91c01b53975df2e1d0a1054f9
sakshidulwani <dulwanisakshi@gmail.com>
2021-04-16 17:04:46 +0530

    First Commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React,{useState} from 'react';
+import {Link} from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <div>
+            <nav className="navbar">
+                <div className="navbar-container">
+            <Link to="/" className="navbar-logo">
+                TRVL <i className="fab fa-typo3"/>  
+            </Link>
+            <div className='menu-icon'>
+                <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+
+            </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
